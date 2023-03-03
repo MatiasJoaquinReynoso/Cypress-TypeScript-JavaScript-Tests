e653eaf88d015de8b59537e27a8498984d7b9e48
MatiasJoaquinReynoso <matiasjoaquinreynoso@gmail.com>
2023-03-03 12:07:10 -0300

    API Spying exercises added

--- cypress/e2e/12-links.cy.ts
+++ cypress/e2e/12-links.cy.ts
@@ -13,7 +13,7 @@ describe("Dealing with links that opens a new tab", () => {
       });
     });
   });
-  
+
   describe("Incercepting API(SPYING) requests after clicking on a button", () => {
     beforeEach(() => {
       cy.visit(`${Cypress.env("demoQA")}/links`);
@@ -28,16 +28,30 @@ describe("Dealing with links that opens a new tab", () => {
       });
     });
   });
-  
-  describe.only("Basics using the internet app", () => {
+
+  describe("Basics using the internet app", () => {
     beforeEach(() => {
       cy.visit(`${Cypress.env("theInternet")}/redirector`);
     });
-  
     it("Internal Link redirect validation", () => {
       cy.get("#redirect").click();
       cy.url().then((url) => {
         expect(url).to.be.equal(`${Cypress.env("theInternet")}/status_codes`);
+      });
+    });
+  });
+
+  describe.only("Incercepting API(SPYING) requests after clicking on a button", () => {
+    beforeEach(() => {
+      cy.visit(`${Cypress.env("theInternet")}/status_codes`);
+      cy.intercept("GET", `${Cypress.env("theInternet")}/status_codes/200`).as("linkStatus");
+    });
+    it("First approach, not click on the link", () => {
+      cy.get(':nth-child(1) > a').click();
+      cy.wait("@linkStatus").then((request) => {
+        cy.log("This is the request intercepted", request);
+        expect(request.response.statusCode).to.be.equal(200);
+        expect(request.response.statusMessage).to.be.equal('OK');
       });
     });
   });
