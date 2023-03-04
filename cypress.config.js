e986cc5cb44878a595c2fbb65dc8710c507e1938
MatiasJoaquinReynoso <matiasjoaquinreynoso@gmail.com>
2023-03-04 10:42:29 -0300

    Imgs & Files exercises added

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
-const { defineConfig } = require("cypress");
+// declare var require: any
+import { defineConfig } from "cypress";
+//Verify download import
+const { isFileExist, findFiles } = require("cy-verify-downloads");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://uitestingplayground.com",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      //Verify download import
+      on("task", { isFileExist, findFiles });
     },
     env:{
       demoVar: "Hello from the Cypress.Config.Ts",
---
+++ cypress/support/commands.ts
@@ -0,0 +1,3 @@
+import 'cypress-file-upload';
+require("cy-verify-downloads").addCustomCommand();
+/// <reference types="cypress" />
